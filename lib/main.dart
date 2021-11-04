0f977bf82819ecace5b8ce706bb00e02b5f56f22
Percy camala <093157@unsaac.edu.pe>
2021-11-04 00:39:10 -0500

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,7 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         debugShowMaterialGrid: false,
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
-            // primarySwatch: Color.white,
-            primaryColor: Colors.blue),
+        theme: ThemeData(primaryColor: Colors.blue),
         home: DefaultTabController(
           length: 3,
           child: Scaffold(
